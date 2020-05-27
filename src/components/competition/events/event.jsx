621c56d9623f0d37b57ba4575b9769c93bd7aba8
Guido Pujadas <guidopujadas@hotmail.com>
2020-05-27 19:23:13 -0300

    delete imports

--- src/components/competition/events/event.jsx
+++ src/components/competition/events/event.jsx
@@ -11,14 +11,6 @@ import _ from 'lodash';
 import constants from 'constants.js'
 import eventStyles from './eventStyles'
 import MenuItem from '@material-ui/core/MenuItem'
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
 import GenericTable from 'components/helpers/genericTable/genericTable'
 
  const Event = React.forwardRef((props, ref) => {
